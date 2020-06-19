8cdfd352639c48fa3d2893bc91355f465ee361e3
fried-taters <treychaffin@gmail.com>
2020-06-19 13:19:15 -0500

    updated PID values from M303 autotuning

--- Configuration.h
+++ Configuration.h
@@ -491,9 +491,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 27.86
+  #define DEFAULT_Ki 2.50
+  #define DEFAULT_Kd 77.61
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
@@ -547,9 +547,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 60.94
+  #define DEFAULT_bedKi 11.88
+  #define DEFAULT_bedKd 208.44
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
